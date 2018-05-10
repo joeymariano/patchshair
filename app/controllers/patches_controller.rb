ae11ca1bf7c7a035cfc413148d1072739a71bb25
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-05-10 04:29:16 +0000

    useing nested resource for patches

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -1,23 +1,28 @@
 class PatchesController < ApplicationController
 
   def show
+    @user = User.find(params[:user_id])
     @patch = Patch.find(params[:id])
   end
 
   def new
+    @user = User.find(params[:user_id])
     @patch = Patch.new
   end
 
   def create
     user = User.find(session[:user_id])
     patch = Patch.new
-    patch.file = params[:patch][:file]
-    patch.name = params[:patch][:name]
+    patch.file = params[:post][:file]
+    patch.name = params[:post][:name]
     user.patches << patch
     redirect_to user_path(user)
   end
 
   def destroy
-
+    user = User.find(session[:user_id])
+    patch = Patch.find(params[:id])
+    patch.destroy
+    redirect_to user_path(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'welcome#index'
 
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create] do
+    resources :patches, only: [:new, :create, :show]
+    get 'user/:id/patches/delete', to: 'patches#destroy', as: 'destroy_patch'
+  end
 
   get '/login', to: 'sessions#new'
 
   get '/logout', to: 'sessions#destroy'
 
   resources :sessions, only: [:create]
-
-  resources :patches, only: [:new, :create, :show]
 end
