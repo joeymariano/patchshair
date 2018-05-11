feb978f471c19a783a440ac3526f67b6e1f69fec
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-05-11 00:52:09 +0000

    added user index and bells to show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'welcome#index'
 
-  resources :users, only: [:show, :new, :create] do
+  resources :users, only: [:index, :show, :new, :create] do
     resources :patches, only: [:new, :create, :show]
     get 'user/:id/patches/delete', to: 'patches#destroy', as: 'destroy_patch'
   end
