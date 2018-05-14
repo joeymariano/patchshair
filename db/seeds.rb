d967a9d4b2d3745eebcd01da08e98f9f969dd7ed
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-13 22:29:25 -0400

    added category to patch show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-User.create(username: 'joey', password: 'joey', password_confirmation: 'joey')
+joey = User.create(username: 'joey', password: 'joeyjoey', password_confirmation: 'joeyjoey')
+
+patch = Patch.create(name: 'cool', game: '', original: true)
+
+bass = Category.create(name: 'bass')
+
+joey.patches << patch
+
+patch.categories << bass
