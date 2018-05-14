d74f9cdca18c511be2a165fd62843db14934753f
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-14 14:23:45 -0400

    added category index view and link

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-
+    @categories = Category.all
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,12 @@ joey = User.create(username: 'joey', password: 'joeyjoey', password_confirmation
 
 patch = Patch.create(name: 'cool', game: '', original: true)
 
-bass = Category.create(name: 'bass')
+bass = Category.create(name: 'Bass')
+lead = Category.create(name: 'Lead')
+pad = Category.create(name: 'Pad')
+percussion = Category.create(name: 'Percussion')
+arpeggio = Category.create(name: 'Arpeggio')
+noise = Category.create(name: 'Noise')
 
 joey.patches << patch
 
