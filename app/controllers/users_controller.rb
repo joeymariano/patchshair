3c9ebdda472b6b5b14bf42cc4d4554e7ce7f159f
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-15 21:53:54 -0400

    error messages on user sign up are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
