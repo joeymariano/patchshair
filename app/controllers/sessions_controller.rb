0cd7353d2c289a23e31ec58e54e3e4f7b8524ad5
nmlstyl <joey.michalina.mariano@gmail.com>
2018-05-16 21:53:18 -0400

    omniauth now fully workin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,19 +5,33 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
-    @user = User.find_by(username: params[:session][:username])
-    if @user && @user.authenticate(params[:session][:password])
+    if auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.password = SecureRandom.hex(4)
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:notice] = 'Oh No!  Login Errors.'
-      render :new
+      @user = User.find_by(username: params[:session][:username])
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:notice] = 'Oh No!  Login Errors.'
+        render :new
+      end
     end
   end
 
   def destroy
     session.delete("user_id")
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515135704) do
+ActiveRecord::Schema.define(version: 20180517010927) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180515135704) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
   end
 
 end
