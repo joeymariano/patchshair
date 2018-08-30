1d4bcb38e5831747b29a64d997ab918c214139b6
nmlstyl <joey.michalina.mariano@gmail.com>
2018-08-29 23:27:02 -0400

    displays all comments and can add them to patch page

--- app/serializers/patch_serializer.rb
+++ app/serializers/patch_serializer.rb
@@ -2,4 +2,5 @@ class PatchSerializer < ActiveModel::Serializer
   attributes :id, :name, :file, :game, :original
 
   has_many :comments
+  # has_many :users, :through => :comments
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
 
   has_many :comments
+  # has_many :patches, :through => :comments
 end
