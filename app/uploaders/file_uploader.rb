4655887ae2f6942768114e3db9b2ac1d3bc62981
joeymariano <joey.michalina.mariano@gmail.com>
2019-01-03 02:39:46 -0500

    trying cloudcube heroku addon

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -10,7 +10,12 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.development?
+    "public/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+    if Rails.env.production?
+    "e0xtnswr9ixv/public/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
