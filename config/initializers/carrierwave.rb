9f888e01d8481c0424db7f0057cfa42ce2cb0a6e
joeymariano <joey.michalina.mariano@gmail.com>
2019-01-04 22:39:24 -0500

    updated S3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
       aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+      region: ENV["S3_REGION"]
     }
     config.fog_directory  = ENV["AWS_BUCKET"]              # required
   else
